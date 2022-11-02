a91c35163330b59f3e10bc525285231494b6e59b
Daniel Alas <81212794+DannyAlas@users.noreply.github.com>
2022-11-01 18:49:54 -0600

    update " cause i'm anal

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 module.exports = {
     app: {
-        token: "MTAzNDQ2NzcxMjMwNDQzNTIwMA.GYZ2mf.IPgkVfqen7_xWHmQLbT7edtLFYe9iEiT8i_y1I",
+        token: 'MTAzNDQ2NzcxMjMwNDQzNTIwMA.GYZ2mf.IPgkVfqen7_xWHmQLbT7edtLFYe9iEiT8i_y1I',
         playing: 'by Jazzy',
         global: true,
         guild: '762098988592791552'
